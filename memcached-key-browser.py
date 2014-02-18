15d58c35081e6cd647ec0d119377a253eb7afe99
David Lam <d@dlam.me>
2014-02-17 23:53:55 -0800

    clean up some stuff, only using tk.Listbox now

--- memcached-key-browser.py
+++ memcached-key-browser.py
@@ -7,7 +7,8 @@
         http://effbot.org/tkinterbook/
         http://effbot.org/tkinterbook/pack.htm
         http://effbot.org/tkinterbook/grid.htm
-
+        http://effbot.org/tkinterbook/listbox.htm
+       http://stackoverflow.com/questions/8647735/tkinter-listbox
 
         Q. How to center a window on screen?
         A. http://stackoverflow.com/questions/14910858/
@@ -20,24 +21,13 @@
 
             http://stackoverflow.com/questions/579687/how-do-i-copy-a-string-to-the-clipboard-on-windows-using-python/4203897#4203897
 
-
 """
-
-
 import Tkinter as tk
 from ScrolledText import ScrolledText
 from sys import platform as _platform
 import os
 import telnetlib
 import time
-
-USE_LISTBOX = True
-"""http://effbot.org/tkinterbook/listbox.htm
-   http://stackoverflow.com/questions/8647735/tkinter-listbox
-"""
-
-USE_GRID = False
-"""http://effbot.org/tkinterbook/grid.htm"""
 
 STATS_ITEM_HELP_MSG = """
 Format:
@@ -47,12 +37,10 @@ Format:
 """
 
 MEMCACHED_END = "END"
-
 popups = list()
 
 def format_epoch_timestamp(ts):
     return time.strftime('%m/%d/%Y %I:%M %p',  time.gmtime(float(ts)))
-
 
 def clear_popups(foo=None):
     for p in popups:
@@ -103,13 +91,11 @@ for line in items.split("\r\n"):
 
 for i in range(3):      # print some horizontal separator bars
     print '=' * 80
-
 print """
 Epoch Time:  {0}
 
-""".format(int(time.time()))
-
-print STATS_ITEM_HELP_MSG
+{1}
+""".format(int(time.time()), STATS_ITEM_HELP_MSG)
 
 root = tk.Tk()
 root.wm_title('Browsing keys in memcached at {0}:{1}   (press Q to quit)  '.format(tn.host, tn.port))
@@ -131,53 +117,44 @@ def quit(event):
     root.quit()
 root.bind('<Q>', quit)
 
-if USE_LISTBOX:
-    frame = tk.Frame(root)
-    frame.pack(fill=tk.BOTH, expand=tk.YES)
-    listbox = tk.Listbox(frame)
-    listbox.configure(background='HotPink1')
-    listbox.pack(fill=tk.BOTH, expand=tk.YES)
-    for i, line in enumerate(output.split("\n")):
-        listbox.insert(i, line)
 
-    scrollbar = tk.Scrollbar(listbox, orient=tk.VERTICAL)
-    scrollbar.config(command=listbox.yview)
-    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-    listbox.config(yscrollcommand=scrollbar.set)  # moving one, moves the other
+# ...now build the UI!
 
-    def handleClick(event):  #   event is a tkinter.Event
+frame = tk.Frame(root)
+frame.pack(fill=tk.BOTH, expand=tk.YES)
+listbox = tk.Listbox(frame)
+listbox.configure(background='HotPink1')
+listbox.pack(fill=tk.BOTH, expand=tk.YES)
+for i, line in enumerate(output.split("\n")):
+    listbox.insert(i, line)
+
+scrollbar = tk.Scrollbar(listbox, orient=tk.VERTICAL)
+scrollbar.config(command=listbox.yview)
+scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+listbox.config(yscrollcommand=scrollbar.set)  # moving one, moves the other
+
+def handleClick(event):  #   event is a tkinter.Event
 #         index = listbox.curselection()
-        index = event.widget.curselection()
-        cachedump_line = listbox.get(index)
+    index = event.widget.curselection()
+    cachedump_line = listbox.get(index)
 
-        toks = cachedump_line.split()
-        try:
-            if toks[0] == 'ITEM':
-                key = toks[1]
-                size_and_expiry = toks[2:]
-                size = size_and_expiry[0]
-                expiry = size_and_expiry[2]
-                tn.write("get {0}\r\n".format(key))
-                value = tn.read_until(MEMCACHED_END)
-                print("*** value of key '{0}' which expires {1} is... {2}".format(
-                      key, format_epoch_timestamp(expiry), value))
-                open_popup(value.decode('unicode-escape'),
-                           'Expires ' + format_epoch_timestamp(expiry))
+    toks = cachedump_line.split()
+    try:
+        if toks[0] == 'ITEM':
+            key = toks[1]
+            size_and_expiry = toks[2:]
+            size = size_and_expiry[0]
+            expiry = size_and_expiry[2]
+            tn.write("get {0}\r\n".format(key))
+            value = tn.read_until(MEMCACHED_END)
+            print("*** value of key '{0}' which expires {1} is... {2}".format(
+                  key, format_epoch_timestamp(expiry), value))
+            open_popup(value.decode('unicode-escape'),
+                       'Expires ' + format_epoch_timestamp(expiry))
 
-        except IndexError:
-            pass
+    except IndexError:
+        pass
 
-    listbox.bind('<<ListboxSelect>>', handleClick)
-
-
-elif USE_GRID:
-    for i, line in enumerate(output.split("\n")):
-        tk.Label(root, text=line).grid(row=i, column=1)
-else:
-    #  http://stackoverflow.com/questions/17657212/how-to-code-the-tkinter-scrolledtext-module
-    keys_display = ScrolledText(root, width=400, height=400,)
-    keys_display.pack(side=tk.LEFT, padx=5, pady=5, fill=tk.BOTH, expand=True)
-    keys_display.insert(tk.INSERT, output)
-
+listbox.bind('<<ListboxSelect>>', handleClick)
 
 root.mainloop()
